3f2fcd0b7684aa730159c223ad543f2bd8160d77
Johannes Raggam <thetetet@gmail.com>
2022-06-08 11:42:15 +0200

    maint(Build): Extend jest.config.js from Patternslib and reuse their setupTests file too.

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,6 @@
-module.exports = {
-    rootDir: "./src",
-    setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
-    watchPlugins: ["jest-watch-typeahead/filename", "jest-watch-typeahead/testname"],
-    transform: {
-        "^.+\\.[t|j]sx?$": "babel-jest",
-    },
-    moduleNameMapper: {
-        "\\.(css|less|sass|scss)$": "identity-obj-proxy",
-    },
-    testEnvironment: "jsdom",
-    transformIgnorePatterns: [
-        "/node_modules/(?!.*(patternslib|codejar)/*).+\\.[t|j]sx?$",
-    ],
-};
+const defaults = require("@patternslib/patternslib/jest.config.js");
+defaults.transformIgnorePatterns = [
+    "/node_modules/(?!.*(patternslib|codejar)/*).+\\.[t|j]sx?$",
+];
+
+module.exports = defaults;
