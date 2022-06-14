e0261e964f864e54507c49e7e23a5a771f972487
Johannes Raggam <thetetet@gmail.com>
2022-06-14 18:45:33 +0200

    breaking: Extend jest config from @patternslib/dev.

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
-const defaults = require("@patternslib/patternslib/jest.config.js");
+const defaults = require("@patternslib/dev/jest.config.js");
 defaults.transformIgnorePatterns = [
     "/node_modules/(?!.*(patternslib|codejar)/*).+\\.[t|j]sx?$",
 ];
